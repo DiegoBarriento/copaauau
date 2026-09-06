b5d66606f6f626e8dcb66c1161dcab54872040c6
unknow-cleaner <diego.barriento@outlook.com>
2026-09-06 10:26:19 -0300

    file locatiopn changed

--- src/components/BotaoRepetidas/index.jsx
+++ src/components/BotaoRepetidas/index.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Carregando from "../Carregando";
 
 
 export default function BotaoRepetidas(props) {
     const { figurinha} = props;
     const [qtdRepetida, setQtdRepetida] = useState(figurinha.QtdRepetidas);
+    const [carregando, setCarregando] = useState(false);
     // teste use effect((AI)=>{
     //     setQtdRepetida(figurinha.QtdRepetidas);
     // }, [figurinha.QtdRepetidas]);
     function atualizarNoServidor(novaQuantidade) {
+        setCarregando(true);
         axios.post("http://localhost/copaauau/api/atualizarquantidade.php", 
         {
             /* conteudo do corpo JSON da requisicão */
@@ -31,6 +34,7 @@ export default function BotaoRepetidas(props) {
         })
         .finally(function () {
         // O que fazer independente de ter dado erro ou não
+        setCarregando(false);
         });
     }
 
@@ -52,17 +56,20 @@ export default function BotaoRepetidas(props) {
     }
 
     return (
-        <section className="figurinhaRep">
-                <p className="numero">{figurinha.Codigo}</p>
-                <div>
-                    <span className="qtdAtual">{qtdRepetida}</span>
-                    <div className="areaBotoesQuantidade">
-                        <button onClick={diminuirQuantidade}>-</button>
-                        <button onClick={aumentarQuantidade}>+</button>
+        <>
+            {carregando ? <Carregando/> : null}
+            <section className="figurinhaRep">
+                    <p className="numero">{figurinha.Codigo}</p>
+                    <div>
+                        <span className="qtdAtual">{qtdRepetida}</span>
+                        <div className="areaBotoesQuantidade">
+                            <button onClick={diminuirQuantidade}>-</button>
+                            <button onClick={aumentarQuantidade}>+</button>
+                        </div>
                     </div>
-                </div>
-                <p className="nome">{figurinha.Nome}</p>
-        </section>
+                    <p className="nome">{figurinha.Nome}</p>
+            </section>
+        </>
     )
 
 }
